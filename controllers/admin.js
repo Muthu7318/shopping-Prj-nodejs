3f169340d6a6f881e523ca0fe63716fc69cc7afe
muthukumar <muthumarshal@gmail.com>
2023-05-13 17:03:39 +0530

    prepopulating the data for edit page

--- controllers/admin.js
+++ controllers/admin.js
@@ -5,6 +5,7 @@ exports.getAddProduct = (req, res, next) => {
   res.render("admin/edit-product", {
     pageTitle: "Add Product",
     path: "/admin/add-product",
+    editing: false,
   });
 };
 
@@ -17,13 +18,22 @@ exports.postAddProduct = (req, res, next) => {
 
 exports.getEditProduct = (req, res, next) => {
   const editMode = req.query.edit;
-  if (!!editMode) {
+  console.log("***  admin.js [20] ***", editMode);
+  if (!!!editMode) {
     return res.redirect("/");
   }
-  res.render("admin/edit-product", {
-    pageTitle: "Edit Product",
-    path: "/admin/edit-product",
-    editing: !!editMode,
+
+  const { productId } = req.params;
+  Product.findById(productId, (product) => {
+    if (!product) {
+      return res.redirect("/");
+    }
+    res.render("admin/edit-product", {
+      pageTitle: "Edit Product",
+      path: "/admin/edit-product",
+      editing: !!editMode,
+      product: product,
+    });
   });
 };
 
