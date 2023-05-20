ac4b3910d95764f2306e155fff7ed3ef030ae48f
muthukumar <muthumarshal@gmail.com>
2023-05-20 13:55:17 +0530

    creating and managing dummy user

--- app.js
+++ app.js
@@ -16,6 +16,15 @@ app.set("views", "views");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
+
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 app.use(errorController.get404);
@@ -28,8 +37,20 @@ Product.belongsTo(User, {
 User.hasMany(Product);
 
 sequelize
-  .sync({ force: true })
+  .sync()
   .then((res) => {
+    return User.findByPk(1);
+  })
+  .then((user) => {
+    if (!user) {
+      return User.create({
+        name: "muthu",
+        email: "muthu@gmail.com",
+      });
+    }
+    return user;
+  })
+  .then((user) => {
     app.listen(3000, () => {
       console.log("server ready");
     });
