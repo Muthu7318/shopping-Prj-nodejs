82d3b9fec7f781dde482a3ef090aa1afa0bd682f
muthukumar <muthumarshal@gmail.com>
2023-05-27 19:07:31 +0530

    adding item to cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -80,6 +80,7 @@ exports.getCart = (req, res, next) => {
 
 exports.postCartDeleteProduct = (req, res, next) => {
   const { productId } = req.body;
+
   Product.findById(productId, (product) => {
     if (!product) {
       return res.redirect("/");
@@ -89,12 +90,42 @@ exports.postCartDeleteProduct = (req, res, next) => {
   });
 };
 
-exports.postCard = (req, res, nex) => {
-  const { productId } = req.body;
-  Product.findByPk(productId, (product) => {
-    Cart.addProduct(productId, product.price);
-  });
-  res.redirect("/cart");
+exports.postCart = (req, res, nex) => {
+  const prodId = req.body.productId;
+  let fetchedCart;
+  let newQuantity = 1;
+  req.user
+    .getCart()
+    .then((cart) => {
+      fetchedCart = cart;
+      return cart.getProducts({
+        where: {
+          id: prodId,
+        },
+      });
+    })
+    .then((products) => {
+      let product;
+      if (products.length > 0) {
+        product = products[0];
+      }
+
+      if (product) {
+        const oldQuantity = product.cartItem.quantity;
+        newQuantity = oldQuantity + 1;
+        return product;
+      }
+      return Product.findByPk(prodId);
+    })
+    .then((product) => {
+      return fetchedCart.addProduct(product, {
+        through: { quantity: newQuantity },
+      });
+    })
+    .then(() => {
+      res.redirect("/cart");
+    })
+    .catch((err) => console.log(err));
 };
 
 exports.getCheckout = (req, res, next) => {
