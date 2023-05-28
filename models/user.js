b799e19c40cb64cb99fa5fa5d2db079ac9810802
muthukumar <muthumarshal@gmail.com>
2023-05-28 20:48:21 +0530

    add to cart in mongoose

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const { schema } = require("./product");
 
 const Schema = mongoose.Schema;
 
@@ -27,6 +28,30 @@ const userSchema = new Schema({
     ],
   },
 });
+
+userSchema.methods.addToCart = function (product) {
+  console.log("test--", this);
+  const cartProductIndex = this.cart.items.findIndex((cartProd) => {
+    return cartProd.productId.toString() === product._id.toString();
+  });
+  let newQuantity = 1;
+  const updatedCartItems = [...this.cart.items];
+
+  if (cartProductIndex >= 0) {
+    newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+    updatedCartItems[cartProductIndex].quantity = newQuantity;
+  } else {
+    updatedCartItems.push({
+      productId: product._id,
+      quantity: newQuantity,
+    });
+  }
+  const updatedCart = {
+    items: updatedCartItems,
+  };
+  this.cart = updatedCart;
+  return this.save();
+};
 
 module.exports = mongoose.model("User", userSchema);
 
